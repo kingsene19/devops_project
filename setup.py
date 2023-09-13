6ba2eda50f22bde9fcef86398b36b7da1815c441
kingsene19 <smmassamba@ept.sn>
2023-09-13 22:55:01 +0000

    Modified Pipeline adding coverage and quality gate measurement

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name="devops_project",
+    version="1.0",
+    packages=find_packages(),
+    scripts=["manage.py"],
+)
